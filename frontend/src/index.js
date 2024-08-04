3871bdc33de5b78bc586e451c1ad8a9755864b05
sk7001 <sk7001@srmist.edu.in>
2024-08-04 17:55:23 +0530

    google oauth working

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Toaster } from 'react-hot-toast';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Toaster position='top-center' />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
     <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
